ca10e80b36bd07ef92a1c4ae0e604680594ba557
Lottie Carruthers <lottie.carruthers@gmail.com>
2016-08-18 15:54:02 +0100

    bug fix: edf now sorts into n bins when there are less than 100 samples

--- src/java/cz/cuni/mff/spl/evaluator/graphs/EmpiricalDistributionNew.java
+++ src/java/cz/cuni/mff/spl/evaluator/graphs/EmpiricalDistributionNew.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class EmpiricalDistributionNew{
 	public static final int  DEFAULT_BIN_COUNT = 1000;
-	private final int binCount; 
+	private int binCount; 
 	 
 	public EmpiricalDistributionNew(int binCount){
 		this.binCount = binCount;
@@ -15,47 +15,17 @@ public class EmpiricalDistributionNew{
 		EmpiricalDistributionNew(DEFAULT_BIN_COUNT);
 	}*/
 
-	
-	/*public double[][] load(double[] a){
-
-		int n = a.length;
-
-		Arrays.sort(a);
-		int min = (int)a[0];
-		int max = (int)a[n-1];
-		int range = max - min;
-		int binSize = range/binCount;
-		double[][] output = new double[binCount + 3][2];
-		double inc = 1/(double)n;
-
-		output[0][0] = 0;
-		
-		for (int i = 0; i < n; i++){
-			output[(((int)a[i] - min)/binSize) + 1][0]++;
-		}
-
-		double current = 0; 
-
-		for (int i = 0; i < output.length; i++){
-			if(output[i][0] == 0){
-				output[i][0] = current;
-			}else{
-				current += output[i][0]*inc;
-				output[i][0] = current;
-				
-			}
-			output[i][1] = (i - 1)*binSize + min;
-		}
-
-
-		return output;
-	}*/
-
-
+		/**
+		* Function takes a double array of data and outputs an nx2 arrayList
+		* containing the y and x coordinates [y,x] of the input's empirical 
+		* distribution. 
+		*/
 		public ArrayList<ArrayList<Double>> load(double[] a){
 
 			int n = a.length;
-
+			if (n < 100){
+				binCount = n;
+			}
 			Arrays.sort(a);
 			int min = (int)a[0];
 			int max = (int)a[n-1];
@@ -65,37 +35,38 @@ public class EmpiricalDistributionNew{
 
 			Double[] counts = new Double[binCount+1];
 			double inc = 1/(double)n;
-			
 			for (int i = 0; i < counts.length; i++){
 				counts[i] = 0D;
 			}
-
+			//for each data point, add one to the appropriate bin
 			for (int i = 0; i < n; i++){
 				counts[(int)((a[i] - min)/binSize)]++;
 			}
 			
-
-
 			double current = 0D; 
-
-
+			// the EDF must start a 0 on the x axis before the first data point
 			ArrayList<Double> firstEntry = new ArrayList<Double>();
 			firstEntry.add(0D);
 			firstEntry.add(min - binSize);
 			output.add(firstEntry);
 
+			//for each bin increase the step by 1/n * (number of datapoints in bin)
+			// need to add the point before and after the step to maintain the step 
+			//function, y coordinates - output(i,0), x coordinates - output(i,1)
 			for (int i = 0; i < counts.length; i++){
-				
+				//if no data points in bin, line is flat, no step
 				if (counts[i] == 0){
 					ArrayList<Double> entry = new ArrayList<Double>();
 					entry.add(current);
 					entry.add(Double.valueOf(i*binSize + min));
 					output.add(entry);
 				}else{
+					// add a plot point before the step
 					ArrayList<Double> entry = new ArrayList<Double>();
 					entry.add(current);
 					entry.add(Double.valueOf(i*binSize + min));
 					output.add(entry);
+					// add a plot point after the step
 					current += counts[i]*inc;
 					ArrayList<Double> entry2 = new ArrayList<Double>();
 					entry2.add(current);
